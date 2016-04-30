6a064c7a898af11e7f6abb3ec20baf9752f1c8ba
kilarusravankumar <kilarusravankumar@hotmail.com>
2016-04-29 22:54:21 -0500

    Initial commit

---
+++ Finidingpeak.py
@@ -0,0 +1,19 @@
+import sys
+
+def findPeak(arr):
+    if len(arr)%2==0:
+        nhalf=len(arr)/2
+    else:
+        nhalf=(len(arr)-1)/2
+    if arr[nhalf]<=arr[nhalf+1]:
+        findPeak(arr[nhalf:])
+    elif arr[nhalf]<=arr[nhalf-1]:
+        findpeak(arr[:nhalf])
+    else:
+        print "peak is ",arr[nhalf]
+        
+n=int(raw_input())
+arr=map(int,raw_input().split())
+
+findPeak(arr)
+
